12d53655d0b95d3ce11721e80da2c692c694ec2a
Craig Bishop <craig.bishop@vcatechnology.com>
2019-07-14 15:19:07 +0100

    Simple API example

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,79 @@
 //! An example of serving static assets with Gotham.
 
 extern crate gotham;
+#[macro_use]
+extern crate gotham_derive;
+extern crate hyper;
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
 
 use gotham::handler::assets::FileOptions;
-use gotham::router::builder::{build_simple_router, DefineSingleRoute, DrawRoutes};
+use gotham::router::builder::*;
+use gotham::helpers::http::response::create_response;
+use gotham::middleware::state::StateMiddleware;
+use gotham::pipeline::single::single_pipeline;
+use gotham::pipeline::single_middleware;
+use gotham::router::Router;
+use gotham::state::{FromState, State, StateData};
+
+use hyper::{Body, Response, StatusCode};
+
+use std::sync::{Arc, Mutex};
+
+use serde_json::json;
+
+#[derive(Clone, StateData)]
+struct RequestCounter {
+    count: Arc<Mutex<usize>>,
+}
+
+/// Counter implementation.
+impl RequestCounter {
+    /// Creates a new request counter, setting the base state to `0`.
+    fn new() -> Self {
+        Self {
+            count: Arc::new(Mutex::new(0)),
+        }
+    }
+
+    /// Increments the internal counter state by `1`, and returns the
+    /// new request counter as an atomic operation.
+    fn incr(&self) -> usize {
+        let mut w = self.count.lock().unwrap();
+        *w += 1;
+        *w
+    }
+}
+
+// impl IntoResponse for ApiData {
+//   fn into_response(self, state: &State) -> Response<Body> {
+//     create_response(
+//       state,
+//       StatusCode::OK,
+//       mime::APPLICATION_JSON,
+//       serde_json::to_string(&self).expect("serialized ApiData")
+//     )
+//   }
+// }
+
+fn get_apidata_handler(state: State) -> (State, Response<Body>) {
+  // let data = ApiData {
+  //   count: 5,
+  // };
+
+  // (state, data)
+
+  let res = {
+    let value = RequestCounter::borrow_from(&state).incr();
+    let res = json!({
+      "count": value
+    });
+    create_response(&state, StatusCode::OK, mime::APPLICATION_JSON, res.to_string())
+  };
+
+  (state, res)
+}
 
 pub fn main() {
     let path = std::env::args()
@@ -18,14 +88,19 @@ pub fn main() {
     let mut default = String::new();
     default.push_str(&path);
 
+    let counter = RequestCounter::new();
+    let middleware = StateMiddleware::new(counter);
+    let pipeline = single_middleware(middleware);
+    let (chain, pipelines) = single_pipeline(pipeline);
 
     default += "/index.html";
-    let router = build_simple_router(|route| {        
+    let router = build_router(chain, pipelines, |route| {        
         //route.get("/").to_file(default);
         route.get("/*").to_dir(
             FileOptions::new(&path).build(),
         );
         route.get("/share").to_file(default);
+        route.get("/api").to(get_apidata_handler);
     });
 
     
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,52 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        count: 0
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Testing rust backend API</p>
+        <p>Rust backend says {this.state.data.count}</p>
+        <button onClick={this.queryBackend}>Query</button>
+      </div>
+    );
+  }
+
+  queryBackend = () => {
+    fetch("/api")
+      .then(result => result.json())
+      .then(data => this.setState({data}))
+  }
 }
 
 export default App;
